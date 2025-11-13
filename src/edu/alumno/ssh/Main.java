fe2837f203ebcf28c5e45956eb4dde478a0f6c39
aruimor135 <aruimor135@g.educaand.es>
2025-11-13 10:38:47 +0100

    Proyecto Java creado desde Eclipse y gestionado por terminal

---
+++ src/edu/alumno/ssh/Main.java
@@ -0,0 +1,22 @@
+package edu.alumno.ssh;
+
+import java.util.Random;
+
+public class Main {
+	public static void main(String[] args) {
+		Random random = new Random();
+
+		System.out.println("Números de la Lotería Primitiva:");
+
+		// Genera 5 números principales
+		for (int i = 0; i < 5; i++) {
+			int numero = random.nextInt(49) + 1;
+			System.out.print(numero + " ");
+		}
+
+		System.out.println();
+		// Número complementario
+		int complementario = random.nextInt(49) + 1;
+		System.out.println("Complementario: " + complementario);
+	}
+}
